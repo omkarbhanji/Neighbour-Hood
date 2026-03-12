ff8a6b8c44847f253bf4f2a65884e04963595006
Omkar <bhanjiomkar@gmail.com>
2026-03-12 20:09:25 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,6 @@ const app = require('./app');
 const mongoose = require("mongoose");
 const DB = process.env.MONGODB_URL
 
-// const DB = ${MONGODB_URL}
-    
-
-  //  "mongodb+srv://bhanjiomkar:Sucasa2021@cluster0.2q2oe.mongodb.net/?appName=Cluster0"
-
-  
 
     mongoose.connect(DB).then((con) => {
         console.log("DB connection successful !");
